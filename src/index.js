4048905e5b3f9449ce13065dc5dce81cf94e1916
AlionaMu <34584155+AlionaMu@users.noreply.github.com>
2020-10-09 17:51:38 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function toReadable (number) {
-    var arr = [
+    let arr = [
         [, "one", "two", "three", "four", "five", "six",
             "seven", "eight", "nine", "ten", "eleven",
             "twelve", "thirteen", "fourteen", "fifteen",
@@ -8,20 +8,22 @@ module.exports = function toReadable (number) {
         [,,"twenty", "thirty", "forty", "fifty", "sixty",
             "seventy", "eighty", "ninety"],
     ];
-if (number < 20){
+    let x = number.toString().substring(1,2);
+    let y = number.toString().substring(2,3);
+  if (number == 0)  return "zero"
+  if (number < 20){
     return arr[0][number];
-}
-if (number >= 100) {
-    var x = number.toString().substring(1,2);
-    if (parseInt(number%100) < 20) {
-        return (arr[0][parseInt(number/100)] + ' hundred ' + arr[0][parseInt(number%100)]);
+  }
+  if (number >= 100) {
+    if (x == 0 && y == 0) return arr[0][parseInt(number/100)] + ' hundred'
+    else if (number%100 < 20) {
+        return (arr[0][parseInt(number/100)] + ' hundred ' + arr[0][number%100]);
     }
-    else if (parseInt(number%100) >= 0) {
-        return (arr[0][parseInt(number/100)] + ' hundred ' + arr[1][x] + ' ' + arr[0][parseInt(number%10)]);
+    else if (number%100 >= 20 ) {
+        return (y == 0?  arr[0][parseInt(number/100)] + ' hundred ' + arr[1][x]: arr[0][parseInt(number/100)] + ' hundred ' + arr[1][x] + ' ' + arr[0][number%10]);
     }
     else {return ('');}
-}
-return  (arr[1][parseInt(number/10)] + ' ' + (parseInt(number%10)? arr[0][parseInt(number%10)]:'' ));
-}
-
+  }
+  return  (x == 0? arr[1][parseInt(number/10)]: arr[1][parseInt(number/10)] + ' ' + (number%10? arr[0][number%10]:'' ));
+  }
 
